54324b6f1b11e78150f2afac697fcb2aa13ab554
danielcbranco <daniel-cb1@hotmail.com>
2021-06-18 17:53:52 -0300

    n sei o commit

---
+++ src/pages/api/_lib/manageSubscription.ts
@@ -0,0 +1,35 @@
+import { query as q } from 'faunadb';
+import { fauna } from "../../../services/fauna";
+import { stripe } from '../../../services/stripe';
+
+export async function saveSubscription(
+  subscriptionId: string,
+  costumerId: string,
+) {
+  const userRef = await fauna.query(
+  q.Select(
+    "ref",
+    q.Get(
+      q.Match(
+        q.Index('user_by_stripe_customer_id'),
+        costumerId
+      )
+    )
+  )
+)
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id,
+  }
+
+  await fauna.query(
+    q.Create(
+      q.Collection('subscriptions'),
+     { data: subscriptionData }
+    )
+  )
+}
