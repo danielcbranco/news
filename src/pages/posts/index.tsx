3e37643f1d16ef5ad1a8509c02220a6ec3f79469
danielcbranco <daniel-cb1@hotmail.com>
2021-06-24 12:10:58 -0300

    commit quintazada

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 import styles from './styles.module.scss';
 import Prismic from '@prismicio/client'
+import Link from 'next/link'
 import { RichText } from 'prismic-dom'
 import { getPrismicClient } from '../../services/prismic';
 import { GetStaticProps } from 'next';
+
 
 type Post = {
   slug: string;
@@ -26,11 +28,13 @@ export default function Posts({ posts }) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
